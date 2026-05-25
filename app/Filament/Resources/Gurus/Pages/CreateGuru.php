c5e26ac60e9d02c7170d858122e5ab714eae0185
Alifudin Alfarizi <alif.alfarizi@etb.co.id>
2026-05-25 09:49:27 +0700

    add: redirect to admin/login

--- app/Filament/Resources/Gurus/Pages/CreateGuru.php
+++ app/Filament/Resources/Gurus/Pages/CreateGuru.php
@@ -3,9 +3,41 @@
 namespace App\Filament\Resources\Gurus\Pages;
 
 use App\Filament\Resources\Gurus\GuruResource;
+use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
 
 class CreateGuru extends CreateRecord
 {
     protected static string $resource = GuruResource::class;
+
+    /**
+     * Mutate data sebelum create guru
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        /**
+         * Create akun user guru
+         */
+        $user = User::create([
+            'name' => $data['nama'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => 'guru',
+        ]);
+
+        /**
+         * Relasikan ke guru
+         */
+        $data['user_id'] = $user->id;
+
+        /**
+         * Hapus field sementara
+         * karena bukan column gurus
+         */
+        unset($data['email']);
+        unset($data['password']);
+
+        return $data;
+    }
 }
--- app/Filament/Resources/Gurus/Schemas/GuruForm.php
+++ app/Filament/Resources/Gurus/Schemas/GuruForm.php
@@ -2,33 +2,79 @@
 
 namespace App\Filament\Resources\Gurus\Schemas;
 
+use Filament\Schemas\Schema;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Schemas\Schema;
 use Filament\Forms\Components\CheckboxList;
+use Filament\Schemas\Components\Section;
 
 class GuruForm
 {
-
-
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
+
+                /**
+                 * NIP guru
+                 */
                 TextInput::make('nip')
                     ->required(),
 
+                /**
+                 * Nama guru
+                 */
                 TextInput::make('nama')
                     ->required(),
 
+                /**
+                 * Nomor HP
+                 */
                 TextInput::make('no_hp'),
 
+                /**
+                 * Alamat
+                 */
                 Textarea::make('alamat'),
 
+                /**
+                 * Relasi guru ↔ mata pelajaran
+                 */
                 CheckboxList::make('mataPelajarans')
                     ->relationship('mataPelajarans', 'nama')
                     ->columns(2)
                     ->label('Mata Pelajaran'),
+
+                /**
+                 * Section akun login
+                 *
+                 * HANYA tampil saat create
+                 * supaya tidak bentrok state edit/create
+                 */
+                Section::make('Akun Login')
+
+                    ->visibleOn('create')
+
+                    ->schema([
+
+                        /**
+                         * Email login guru
+                         */
+                        TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->dehydrated(false)
+                            ->autocomplete(false),
+
+                        /**
+                         * Password login guru
+                         */
+                        TextInput::make('password')
+                            ->password()
+                            ->required()
+                            ->dehydrated(false)
+                            ->autocomplete(false),
+                    ]),
             ]);
     }
 }
